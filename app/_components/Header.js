0631cf01678711194a54e8c9c2e717c0f8f2f113
Bruce Hung <brucewmhung@gmail.com>
2025-09-22 22:34:34 +0800

    Header adjustment z-10

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -4,7 +4,7 @@ import BurgerMenu from './BurgerMenu';
 
 function Header() {
   return (
-    <div className='w-full my-5 px-8'>
+    <div className='w-full my-5 px-8 z-10'>
       <div className='flex justify-between items-center max-w-7xl mx-auto'>
         <Logo />
         <Navigation />
